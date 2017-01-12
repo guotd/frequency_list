5f14f29d789be5eed96ccbd48c0feaa2c4cefde5
stephen <neuguotd@gmail.com>
2017-01-12 08:10:57 +0800

    add word variation

--- README.md
+++ README.md
@@ -1,4 +1,35 @@
 # frequency_list
 A shell script to produce word frequency list of English document
 
-# add a comment
+缘起
+============
+媳妇的课后作业要求多人共同翻译一篇英文文档，如果可以找出论文中的高频核心词汇，在大家动手翻译前确定他们的意义，并对它们的翻译词汇达成一致，就可以有效提高最后产出的翻译全文的质量。
+
+思路
+============
+输入：待处理英文文档
+输出：按词频排序的单词列表
+算法：
+1、去掉标点符号 tr -d '[:punct:]' | tr '\r\n' ' ' | tr -d '[:cntrl:]'
+2、分解为空格分隔的单词（去掉空行和多余空格）tr '\r\n' ' '
+3、去掉停词（stop words）,来源：http://www.ranks.nl/stopwords
+4、语言处理：变形词、复词等变为词根形式，参考12dicts（http://wordlist.aspell.net/12dicts-readme/）词表，生成英语常用词的变体词表
+     从12dicts文档中得到常用词的变体词库：
+::
+     cat 2+2+3lem.txt | sed 's/ -> \[[a-z]*,*\]//g' > lem
+     cat lem | xxd -p | tr -d '\n' | sed 's/0d0a20202020/3d/g' | xxd -r -p > word_variation.txt
+5、统计词频并排序输出
+
+参考
+============
+开源的另一个词频统计脚步：https://github.com/Enaunimes/freeq
+获取停词列表：http://www.ranks.nl/stopwords
+变体词：http://wordlist.aspell.net/12dicts-readme/
+
+杂项MISC
+============
+运行环境：依赖GNU command line tools(grep, awk, sed, etc.)
+使用
+::
+./frequence_list.sh -i <input_file> -o <output_file>
+License：BSD-2-Clause license
--- frequence_list.sh
+++ frequence_list.sh
@@ -2,6 +2,7 @@
 
 # VARIABLES
 STOP_WORD_FILE=$(dirname $0)/stop_words.txt
+VARIATION_WORD_FILE=$(dirname $0)/word_variation.txt
 
 function help() {
   echo "Usage $0 [-h] -i <input file> -o <output file>"
@@ -21,12 +22,12 @@ while getopts :i:o:h opt; do
     esac
   done
 
-if [ "$input_file" = "" -o ! -f "$input_file" -o ! -f "$STOP_WORD_FILE" ]; then
+if [ "$input_file" = "" -o ! -f "$input_file" -o ! -f "$STOP_WORD_FILE" -o ! -f "$output_file" ]; then
   help
   exit 1
 fi
 
-# convert punctation to space
+# convert punctuation to space
 tmp_res=`cat $input_file | tr '[:punct:]' ' '`
 
 # remove non-ascii characters
@@ -36,10 +37,13 @@ tmp_res=`echo "$tmp_res" | tr -dc '[\00-\176]'`
 tmp_res=`echo "$tmp_res" | tr '\r\n' ' ' | tr '[:cntrl:]' ' ' | tr -s ' ' | tr ' ' '\n'`
 
 # count word frequency and sort
-tmp_res=`echo "$tmp_res" | awk '{for(i=1;i<=NF;i++) freq[$i]++} END {for(a in freq) print freq[a],a}' | sort -nr`
+tmp_res=`echo "$tmp_res" | awk '{for(i=1;i<=NF;i++) freq[$i]++} END {for(a in freq) print freq[a],a}' `
 
 # remove stop words
-tmp_res=`echo "$tmp_res" | grep -v -w -f $STOP_WORD_FILE`
+tmp_res=`echo "$tmp_res" | grep -v -w -i -f $STOP_WORD_FILE`
+
+# process result list with word variation list
+tmp_res=`awk 'ARGIND==1{len=split($2,v_arr,",");for(i=1;i<=len;i++){v_words[v_arr[i]]=$1}} ARGIND==2{if(v_words[$2]!=""){w_arr[v_words[$2]]+=$1}else{w_arr[$2]+=$1}} END{for(item in w_arr)print w_arr[item],item }' $VARIATION_WORD_FILE <(echo "$tmp_res") | sort -nr`
 
 echo "$tmp_res" > $output_file
 echo "Done! please see the result in file $output_file"
